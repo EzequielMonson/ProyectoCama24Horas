07ea0cab58d4182190d46112163de3d27e71464b
EzequielMonson <ezemonson2024@gmail.com>
2024-05-21 18:24:01 -0300

    modifique la seccion productos

--- templates/scripts.js
+++ templates/scripts.js
@@ -1,41 +1,89 @@
+// Función para guardar el carrito en el localStorage
+function guardarCarritoEnLocalStorage() {
+    // Convertir cada producto del carrito a un objeto plano con información adicional del tipo
+    var carritoParaGuardar = carrito.map(function(producto) {
+        return {
+            tipo: producto.constructor.name, // Agregar el tipo de producto al objeto
+            datos: producto
+        };
+    });
+    localStorage.setItem('carrito', JSON.stringify(carritoParaGuardar));
+}
+
+// Función para obtener el carrito desde el localStorage
+function obtenerCarritoDesdeLocalStorage() {
+    var carritoGuardado = localStorage.getItem('carrito');
+    if (carritoGuardado) {
+        var carritoJSON = JSON.parse(carritoGuardado);
+        carrito = carritoJSON.map(function(item) {
+            var producto;
+            switch(item.tipo) {
+                case 'Colchon':
+                    producto = new Colchon(item.datos.tipoMaterial, item.datos.alto, item.datos.ancho);
+                    break;
+                case 'Cama':
+                    producto = new Cama(item.datos.tipoMaterial, item.datos.alto, item.datos.ancho);
+                    break;
+                case 'Almohada':
+                    producto = new Almohada(item.datos.tipoMaterial, item.datos.alto, item.datos.ancho);
+                    break;
+                default:
+                    throw new Error('Tipo de producto no reconocido');
+            }
+            // Asignar el precio al producto
+            producto.precio = item.datos.precio;
+            return producto;
+        });
+        mostrarProductosEnCarrito(); // Actualizar la lista de productos en el carrito al cargar
+    }
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     // Evento del Overlay, es el menú desplegable
     var menuBtn = document.querySelector('.menu-btn');
     var overlay = document.querySelector('.overlay');
     var navLinks = document.querySelectorAll('.overlay-content .nav-list li a');
+    
     // Abrir el menú al hacer clic en el botón de menú
     menuBtn.addEventListener('click', function() {
         overlay.classList.toggle('active');
     });
+    
     // Cerrar el menú al hacer clic fuera del menú
     overlay.addEventListener('click', function(event) {
         if (event.target === overlay) {
             overlay.classList.remove('active');
         }
     });
+    
     // Cerrar el menú al hacer clic en un enlace de navegación dentro del menú
     navLinks.forEach(function(link) {
         link.addEventListener('click', function() {
             overlay.classList.remove('active');
         });
     });
+    
     // Manejo del overlay del carrito
     var carritoBtn = document.querySelector('.boton-carrito');
     var carritoOverlay = document.querySelector('.overlay-carrito');
     var cerrarCarritoBtn = document.querySelector('.cerrar-carrito');
+    
     carritoBtn.addEventListener('click', function() {
         carritoOverlay.classList.toggle('active');
         mostrarProductosEnCarrito();
     });
+    
     cerrarCarritoBtn.addEventListener('click', function() {
         carritoOverlay.classList.remove('active');
     });
+    
     // Cerrar el carrito al hacer clic fuera del contenido del carrito
     carritoOverlay.addEventListener('click', function(event) {
         if (event.target === carritoOverlay) {
             carritoOverlay.classList.remove('active');
         }
     });
+    
     // Evento para agregar opciones de cama, almohada y colchón
     var elementoMain = document.querySelector("main");
     var btnAgregarOpcionesCama = document.getElementById("agregarOpcionesCama");
@@ -50,12 +98,11 @@ document.addEventListener('DOMContentLoaded', function() {
         
         opciones.forEach(function(opcion, index) {
             codigoHTML += `
-            <label>
+            <label id="opcion-tipo">
                 <input class="radio-tipos" type="radio" id="opcion${index + 1}" name="opcion" value="${opcion}">
                 ${opcion}
             </label>
             <br>
-    
             `;
         });
 
@@ -85,8 +132,8 @@ document.addEventListener('DOMContentLoaded', function() {
             </tbody>
         </table>
         <label id="labelPrecioTotal">Precio :</label>
-        <button id="btn-agregar-carrito"onclick="agregarAlCarrito('${tipo}')">Agregar al carrito</button>
-        </article>
+        <button id="btn-agregar-carrito" onclick="agregarAlCarritoYGuardar('${tipo}')">Agregar al carrito</button>
+        </section>
         `;
         elementoMain.innerHTML = codigoHTML;
 
@@ -94,6 +141,14 @@ document.addEventListener('DOMContentLoaded', function() {
         var botonesTabla = document.querySelectorAll('#tablaMedidas button');
         botonesTabla.forEach(function(boton) {
             boton.addEventListener('click', function() {
+                // Quitar la clase 'boton-seleccionado' de todos los botones
+                botonesTabla.forEach(function(b) {
+                    b.classList.remove('boton-seleccionado');
+                });
+                // Agregar la clase 'boton-seleccionado' al botón clickeado
+                boton.classList.add('boton-seleccionado');
+
+                // Actualizar atributos del botón seleccionado
                 var dimensiones = boton.getAttribute('data-dimensiones').split('x');
                 var precio = boton.getAttribute('data-precio');
                 boton.setAttribute('data-alto', dimensiones[0]);
@@ -120,7 +175,7 @@ document.addEventListener('DOMContentLoaded', function() {
     
     btnAgregarOpcionesAlmohada.addEventListener("click", function() {
         var tipo = "Estilos de almohada";
-        var opciones = ["Almohada de plumas", "Almohada de espuma\nviscoelástica", "Almohada de látex"];
+        var opciones = ["Almohada de plumas", "Almohada de espuma viscoelástica", "Almohada de látex"];
         var medidasPrecios = [
             {tamano: "Pequeña", dimensiones: "50x70", precio: 50000},
             {tamano: "Mediana", dimensiones: "60x80", precio: 70000},
@@ -142,6 +197,8 @@ document.addEventListener('DOMContentLoaded', function() {
         ];
         agregarOpciones(tipo, opciones, medidasPrecios);
     });
+     // Llama a la función para obtener el carrito desde el localStorage al cargar la página
+    obtenerCarritoDesdeLocalStorage();
 });
 
 // Creacion de la lista de productos "Carrito"
@@ -193,11 +250,12 @@ class Almohada extends Producto {
         return `Almohada - ${this.obtenerMedidas()} Diseño: ${this.tipoMaterial} - Precio: $${this.precio}`;
     }
 }
+
 function agregarAlCarrito(tipo) {
     let tipoMaterial = document.querySelector('input[name="opcion"]:checked').value;
     
     // Obtener valores del botón seleccionado
-    var botonSeleccionado = document.querySelector('#tablaMedidas button[data-alto][data-ancho]');
+    var botonSeleccionado = document.querySelector('#tablaMedidas button.boton-seleccionado');
 
     if (!botonSeleccionado) {
         alert('Por favor, selecciona un tamaño de la tabla.');
@@ -224,11 +282,6 @@ function agregarAlCarrito(tipo) {
     alert('Producto agregado al carrito');
 }
 
-
-
-
-
-    
 function quitarDelCarrito(productoSeleccionado) {
     const index = carrito.indexOf(productoSeleccionado);
     if (index > -1) {
@@ -237,8 +290,13 @@ function quitarDelCarrito(productoSeleccionado) {
     } else {
         alert('Producto no encontrado en el carrito');
     }
-    }
-    
+    mostrarProductosEnCarrito(); // Actualizar la lista de productos en el carrito
+}
+
+function calcularPrecioTotal() {
+    return carrito.reduce((total, producto) => total + producto.precio, 0);
+}
+
 function mostrarProductosEnCarrito() {
     var listaProductos = document.querySelector('.overlay-carrito-content ul');
     listaProductos.innerHTML = ''; // Limpiar la lista antes de agregar los productos
@@ -251,10 +309,30 @@ function mostrarProductosEnCarrito() {
         btnQuitar.textContent = 'Quitar';
         btnQuitar.addEventListener('click', function() {
             quitarDelCarrito(producto); // Llamar a la función quitarDelCarrito con el producto actual
-            mostrarProductosEnCarrito(); // Actualizar la lista de productos en el overlay del carrito después de quitar un producto
         });
 
         li.appendChild(btnQuitar);
         listaProductos.appendChild(li);
     });
+
+    var precioTotal = calcularPrecioTotal();
+    var totalElement = document.querySelector('.precio-total');
+    if (!totalElement) {
+        totalElement = document.createElement('p');
+        totalElement.classList.add('precio-total');
+        var overlayCarritoContent = document.querySelector('.overlay-carrito-content');
+        overlayCarritoContent.insertBefore(totalElement, overlayCarritoContent.querySelector('.cerrar-carrito'));
+    }
+    totalElement.textContent = `Precio Total: $${precioTotal.toLocaleString()}`;
+}
+// Función para agregar un producto al carrito y luego guardar el carrito en el localStorage
+function agregarAlCarritoYGuardar(tipo) {
+    agregarAlCarrito(tipo);
+    guardarCarritoEnLocalStorage();
+}
+
+// Función para quitar un producto del carrito y luego guardar el carrito en el localStorage
+function quitarDelCarritoYGuardar(productoSeleccionado) {
+    quitarDelCarrito(productoSeleccionado);
+    guardarCarritoEnLocalStorage();
 }
