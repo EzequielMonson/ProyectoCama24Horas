2aae1999729f25142846ddae55dafa5bd3ec19cc
EzequielMonson <ezemonson2024@gmail.com>
2024-05-22 02:52:33 -0300

    Seccion compra, y productos

--- templates/scripts.js
+++ templates/scripts.js
@@ -39,165 +39,117 @@ function obtenerCarritoDesdeLocalStorage() {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    // Evento del Overlay, es el menú desplegable
+    // Manejo del botón de compra
+    var comprarBtn = document.getElementById('comprar-btn');
+    if (comprarBtn) {
+        comprarBtn.addEventListener('click', function() {
+            if (carrito.length > 0) {
+                window.location.href = 'compras.html';
+            } else {
+                alert('El carrito está vacío. Por favor, agrega productos antes de comprar.');
+            }
+            
+        });
+    } else {
+        console.error('El botón de compra no se encontró.');
+    }
+
+    // Manejo del menú desplegable
     var menuBtn = document.querySelector('.menu-btn');
     var overlay = document.querySelector('.overlay');
     var navLinks = document.querySelectorAll('.overlay-content .nav-list li a');
     
-    // Abrir el menú al hacer clic en el botón de menú
-    menuBtn.addEventListener('click', function() {
-        overlay.classList.toggle('active');
-    });
-    
-    // Cerrar el menú al hacer clic fuera del menú
-    overlay.addEventListener('click', function(event) {
-        if (event.target === overlay) {
-            overlay.classList.remove('active');
-        }
-    });
-    
-    // Cerrar el menú al hacer clic en un enlace de navegación dentro del menú
-    navLinks.forEach(function(link) {
-        link.addEventListener('click', function() {
-            overlay.classList.remove('active');
+    if (menuBtn && overlay && navLinks.length > 0) {
+        menuBtn.addEventListener('click', function() {
+            overlay.classList.toggle('active');
         });
-    });
-    
+        
+        overlay.addEventListener('click', function(event) {
+            if (event.target === overlay) {
+                overlay.classList.remove('active');
+            }
+        });
+        
+        navLinks.forEach(function(link) {
+            link.addEventListener('click', function() {
+                overlay.classList.remove('active');
+            });
+        });
+    } else {
+        console.error('No se encontraron elementos del menú desplegable.');
+    }
+
     // Manejo del overlay del carrito
     var carritoBtn = document.querySelector('.boton-carrito');
     var carritoOverlay = document.querySelector('.overlay-carrito');
     var cerrarCarritoBtn = document.querySelector('.cerrar-carrito');
     
-    carritoBtn.addEventListener('click', function() {
-        carritoOverlay.classList.toggle('active');
-        mostrarProductosEnCarrito();
-    });
-    
-    cerrarCarritoBtn.addEventListener('click', function() {
-        carritoOverlay.classList.remove('active');
-    });
-    
-    // Cerrar el carrito al hacer clic fuera del contenido del carrito
-    carritoOverlay.addEventListener('click', function(event) {
-        if (event.target === carritoOverlay) {
+    if (carritoBtn && carritoOverlay && cerrarCarritoBtn) {
+        carritoBtn.addEventListener('click', function() {
+            carritoOverlay.classList.toggle('active');
+            mostrarProductosEnCarrito();
+        });
+        
+        cerrarCarritoBtn.addEventListener('click', function() {
             carritoOverlay.classList.remove('active');
-        }
-    });
-    
-    // Evento para agregar opciones de cama, almohada y colchón
-    var elementoMain = document.querySelector("main");
+        });
+        
+        carritoOverlay.addEventListener('click', function(event) {
+            if (event.target === carritoOverlay) {
+                carritoOverlay.classList.remove('active');
+            }
+        });
+    } else {
+        console.error('No se encontraron elementos del overlay del carrito.');
+    }
+
+    // Manejo de eventos para agregar opciones de productos
     var btnAgregarOpcionesCama = document.getElementById("agregarOpcionesCama");
     var btnAgregarOpcionesAlmohada = document.getElementById("agregarOpcionesAlmohada");
     var btnAgregarOpcionesColchon = document.getElementById("agregarOpcionesColchon");
 
-    function agregarOpciones(tipo, opciones, medidasPrecios) {
-        var codigoHTML = `
-        <section class="contenedor-modificaciones">
-        <section class="opciones-tipo">
-        <label for="opcion">${tipo}: </label> <br>`;
-        
-        opciones.forEach(function(opcion, index) {
-            codigoHTML += `
-            <label id="opcion-tipo">
-                <input class="radio-tipos" type="radio" id="opcion${index + 1}" name="opcion" value="${opcion}">
-                ${opcion}
-            </label>
-            <br>
-            `;
-        });
-
-        // Tabla de medidas
-        codigoHTML += `
-        </section>
-        <table id="tablaMedidas">
-            <thead>
-                <tr>
-                    <th>Tamaños</th>
-                    <th>Medidas (cm)</th>
-                    <th>Precio</th>
-                </tr>
-            </thead>
-            <tbody>`;
-        
-        medidasPrecios.forEach(function(item) {
-            codigoHTML += `
-            <tr>
-                <td><button class="boton-tabla" data-dimensiones="${item.dimensiones}" data-precio="${item.precio}">${item.tamano}</button></td>
-                <td>${item.dimensiones.replace('x', ' x ')}</td>
-                <td>$${item.precio.toLocaleString()}</td>
-            </tr>`;
-        });
-        
-        codigoHTML += `
-            </tbody>
-        </table>
-        <label id="labelPrecioTotal">Precio :</label>
-        <button id="btn-agregar-carrito" onclick="agregarAlCarritoYGuardar('${tipo}')">Agregar al carrito</button>
-        </section>
-        `;
-        elementoMain.innerHTML = codigoHTML;
-
-        // Agregar eventos a los botones de la tabla
-        var botonesTabla = document.querySelectorAll('#tablaMedidas button');
-        botonesTabla.forEach(function(boton) {
-            boton.addEventListener('click', function() {
-                // Quitar la clase 'boton-seleccionado' de todos los botones
-                botonesTabla.forEach(function(b) {
-                    b.classList.remove('boton-seleccionado');
-                });
-                // Agregar la clase 'boton-seleccionado' al botón clickeado
-                boton.classList.add('boton-seleccionado');
-
-                // Actualizar atributos del botón seleccionado
-                var dimensiones = boton.getAttribute('data-dimensiones').split('x');
-                var precio = boton.getAttribute('data-precio');
-                boton.setAttribute('data-alto', dimensiones[0]);
-                boton.setAttribute('data-ancho', dimensiones[1]);
-                boton.setAttribute('data-profundo', 200); // Assuming a default depth of 200
-                boton.setAttribute('data-precio', precio);
-            });
-        });
-    }
-    
-    btnAgregarOpcionesCama.addEventListener("click", function() {
-        var tipo = "Tipos de maderas";
-        var opciones = ["Pino", "Abeto", "Roble"];
-        var medidasPrecios = [
-            {tamano: "1 plaza", dimensiones: "80x190", precio: 180000},
-            {tamano: "1 y 1/2 plaza", dimensiones: "100x190", precio: 323000},
-            {tamano: "2 plazas", dimensiones: "140x190", precio: 452200},
-            {tamano: "Queen", dimensiones: "160x200", precio: 544000},
-            {tamano: "King", dimensiones: "180x200", precio: 612000},
-            {tamano: "Superking", dimensiones: "200x200", precio: 964000}
-        ];
-        agregarOpciones(tipo, opciones, medidasPrecios);
-    });
-    
-    btnAgregarOpcionesAlmohada.addEventListener("click", function() {
-        var tipo = "Estilos de almohada";
-        var opciones = ["Almohada de plumas", "Almohada de espuma viscoelástica", "Almohada de látex"];
-        var medidasPrecios = [
-            {tamano: "Pequeña", dimensiones: "50x70", precio: 50000},
-            {tamano: "Mediana", dimensiones: "60x80", precio: 70000},
-            {tamano: "Grande", dimensiones: "70x90", precio: 90000}
-        ];
-        agregarOpciones(tipo, opciones, medidasPrecios);
-    });
-
-    btnAgregarOpcionesColchon.addEventListener("click", function() {
-        var tipo = "Diseños de colchón";
-        var opciones = ["Colchón ortopédico", "Colchón de espuma", "Colchón de muelles"];
-        var medidasPrecios = [
+    if (btnAgregarOpcionesCama && btnAgregarOpcionesAlmohada && btnAgregarOpcionesColchon) {
+        btnAgregarOpcionesCama.addEventListener("click", function() {
+            var tipo = "Diseños de colchón";
+            var opciones = ["Colchón ortopédico", "Colchón de espuma", "Colchón de muelles"];
+            var medidasPrecios = [
             {tamano: "1 plaza", dimensiones: "80x190", precio: 250000},
             {tamano: "1 y 1/2 plaza", dimensiones: "100x190", precio: 350000},
             {tamano: "2 plazas", dimensiones: "140x190", precio: 480000},
             {tamano: "Queen", dimensiones: "160x200", precio: 600000},
             {tamano: "King", dimensiones: "180x200", precio: 750000},
             {tamano: "Superking", dimensiones: "200x200", precio: 1000000}
-        ];
-        agregarOpciones(tipo, opciones, medidasPrecios);
-    });
-     // Llama a la función para obtener el carrito desde el localStorage al cargar la página
+            ];
+            agregarOpciones(tipo, opciones, medidasPrecios);
+        });
+
+        btnAgregarOpcionesAlmohada.addEventListener("click", function() {
+            var tipo = "Estilos de almohada";
+            var opciones = ["Almohada de plumas", "Almohada de espuma viscoelástica", "Almohada de látex"];
+            var medidasPrecios = [
+                {tamano: "Pequeña", dimensiones: "50x70", precio: 50000},
+                {tamano: "Mediana", dimensiones: "60x80", precio: 70000},
+                {tamano: "Grande", dimensiones: "70x90", precio: 90000}
+            ];
+            agregarOpciones(tipo, opciones, medidasPrecios);
+        });
+
+        btnAgregarOpcionesColchon.addEventListener("click", function() {
+            var tipo = "Diseños de colchón";
+            var opciones = ["Colchón ortopédico", "Colchón de espuma", "Colchón de muelles"];
+            var medidasPrecios = [
+                {tamano: "1 plaza", dimensiones: "80x190", precio: 250000},
+                {tamano: "1 y 1/2 plaza", dimensiones: "100x190", precio: 350000},
+                {tamano: "2 plazas", dimensiones: "140x190", precio: 480000},
+                {tamano: "Queen", dimensiones: "160x200", precio: 600000},
+                {tamano: "King", dimensiones: "180x200", precio: 750000},
+                {tamano: "Superking", dimensiones: "200x200", precio: 1000000}
+            ];
+            agregarOpciones(tipo, opciones, medidasPrecios);
+        });
+    
+    } 
+    // Llamada a la función para obtener el carrito desde el localStorage al cargar la página
     obtenerCarritoDesdeLocalStorage();
 });
 
@@ -290,6 +242,7 @@ function quitarDelCarrito(productoSeleccionado) {
     } else {
         alert('Producto no encontrado en el carrito');
     }
+    guardarCarritoEnLocalStorage(); // Guardar el carrito actualizado en el localStorage
     mostrarProductosEnCarrito(); // Actualizar la lista de productos en el carrito
 }
 
@@ -299,7 +252,15 @@ function calcularPrecioTotal() {
 
 function mostrarProductosEnCarrito() {
     var listaProductos = document.querySelector('.overlay-carrito-content ul');
-    listaProductos.innerHTML = ''; // Limpiar la lista antes de agregar los productos
+    
+    // Verificar si el elemento del overlay está presente
+    if (!listaProductos) {
+        console.error('El elemento de la lista de productos en el overlay no se encontró.');
+        return;
+    }
+
+    // Limpiar la lista antes de agregar los productos
+    listaProductos.innerHTML = '';
 
     carrito.forEach(function(producto) {
         var li = document.createElement('li');
@@ -308,7 +269,7 @@ function mostrarProductosEnCarrito() {
         var btnQuitar = document.createElement('button');
         btnQuitar.textContent = 'Quitar';
         btnQuitar.addEventListener('click', function() {
-            quitarDelCarrito(producto); // Llamar a la función quitarDelCarrito con el producto actual
+            quitarDelCarritoYGuardar(producto); // Llamar a la función quitarDelCarritoYGuardar con el producto actual
         });
 
         li.appendChild(btnQuitar);
@@ -317,14 +278,28 @@ function mostrarProductosEnCarrito() {
 
     var precioTotal = calcularPrecioTotal();
     var totalElement = document.querySelector('.precio-total');
-    if (!totalElement) {
-        totalElement = document.createElement('p');
-        totalElement.classList.add('precio-total');
-        var overlayCarritoContent = document.querySelector('.overlay-carrito-content');
-        overlayCarritoContent.insertBefore(totalElement, overlayCarritoContent.querySelector('.cerrar-carrito'));
+    var precioFormulario = document.getElementById("precio-total")
+    if (totalElement) {
+        totalElement.textContent = `Precio Total: $${precioTotal.toLocaleString()}`;
     }
-    totalElement.textContent = `Precio Total: $${precioTotal.toLocaleString()}`;
+    else { 
+        if(precioFormulario){
+            precioFormulario.textContent = `Precio Total: $${precioTotal.toLocaleString()}`;
+        }
+        else{
+            console.error('El elemento precio-total no se encontró')
+            return;
+        }
+        
+    }
 }
+    
+    
+
+    
+
+
+
 // Función para agregar un producto al carrito y luego guardar el carrito en el localStorage
 function agregarAlCarritoYGuardar(tipo) {
     agregarAlCarrito(tipo);
@@ -335,4 +310,35 @@ function agregarAlCarritoYGuardar(tipo) {
 function quitarDelCarritoYGuardar(productoSeleccionado) {
     quitarDelCarrito(productoSeleccionado);
     guardarCarritoEnLocalStorage();
+}
+// Función para mostrar el orden de productos en la sección 'listado-productos'
+function mostrarOrdenDeProductos() {
+    var listadoProductos = document.getElementById('listado-productos');
+    
+    if (!listadoProductos) {
+        console.error('El contenedor de listado de productos no se encontró.');
+        return;
+    }
+    
+
+    listadoProductos.innerHTML = ''; // Limpiar el contenido existente
+
+    carrito.forEach(function(producto) {
+        var productoHTML = `
+            <div class="producto">
+                <h3>${producto.constructor.name}</h3>
+                <p>Tipo de material: ${producto.tipoMaterial}</p>
+                <p>Medidas: ${producto.ancho} x ${producto.alto} cm</p>
+                <p>Precio: $${producto.precio.toLocaleString()}</p>
+            </div>
+        `;
+        listadoProductos.innerHTML += productoHTML;
+    var precioTotal = calcularPrecioTotal();
+    var totalElement = document.getElementById('precio-total');
+    if (!totalElement) {
+        console.error('El elemento del precio total no se encontró.');
+        return;
+    }
+    totalElement.textContent = `Precio Total: $${precioTotal.toLocaleString()}`;
+    });
 }
