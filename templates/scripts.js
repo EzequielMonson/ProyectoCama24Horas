1ee2053d89d5960a49060f1714c3d16cc4a6ad52
EzequielMonson <ezemonson2024@gmail.com>
2024-05-22 16:36:09 -0300

    Seccion compra, y productos

--- templates/scripts.js
+++ templates/scripts.js
@@ -208,7 +208,61 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     
     } 
-    
+    var formContacto = document.getElementById("formularioContacto");
+    if (formContacto){
+        formContacto.addEventListener("submit", function(event) {
+            event.preventDefault();
+        
+            // Validación de campos
+            var motivo = document.getElementById("motivoSelect").value;
+            var nombre = document.getElementById("nombre").value;
+            var apellido = document.getElementById("apellido").value;
+            var dni = document.getElementById("dni").value;
+            var telefono = document.getElementById("telefono").value;
+            var email = document.getElementById("email").value;
+            var provincia = document.getElementById("provincia").value;
+            var comentario = document.getElementById("comentario").value;
+        
+            if (motivo === "vacio") {
+                mostrarAlerta("Por favor, seleccione un motivo.");
+                return;
+            }
+        
+            if (nombre === "") {
+                mostrarAlerta("Por favor, introduzca su nombre.");
+                return;
+            }
+        
+            if (apellido === "") {
+                mostrarAlerta("Por favor, introduzca su apellido.");
+                return;
+            }
+        
+            if (dni === "") {
+                mostrarAlerta("Por favor, introduzca su DNI.");
+                return;
+            }
+            if (provincia === "") {
+                mostrarAlerta("Por favor, introduzca su provincia.");
+                return;
+            }
+        
+            if (comentario === "") {
+                mostrarAlerta("Por favor, introduzca un comentario.");
+                return;
+            }
+            if (telefono === "") {
+                mostrarAlerta("Por favor, introduzca un telefono.");
+                return;
+            }
+            if (email === "") {
+                mostrarAlerta("Por favor, introduzca un email.");
+                return;
+            }
+            // Aquí enviarías el formulario o realizarías alguna otra acción
+            alert("Formulario enviado correctamente.");
+        });        
+    }
     // Llamada a la función para obtener el carrito desde el localStorage al cargar la página
     obtenerCarritoDesdeLocalStorage();
 });
